569082a61b4edf53aad51d8f5f9e57c8c91355f2
Vincent Guillemette <vincentguillemette1981@gmail.com>
2019-07-10 23:47:06 -0400

    node + express + socket io

--- src/app/battle/battle.component.ts
+++ src/app/battle/battle.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import * as io from 'socket.io-client';
 import { CharacterActionTypes } from 'src/models/character-action';
 import Eye from 'src/models/characters/eye';
 import FighterNpc from 'src/models/characters/fighter-npc';
 import WhiteMageNpc from 'src/models/characters/white-mage-npc';
 import { BattleService, BattleTeams } from '../battle.service';
+
 
 @Component({
     selector: 'app-battle',
@@ -13,6 +15,7 @@ import { BattleService, BattleTeams } from '../battle.service';
 export class BattleComponent implements OnInit {
     public battleTeams = BattleTeams;
     public characterActionTypes = CharacterActionTypes;
+    public socket: SocketIOClient.Socket = io.connect();
 
     constructor(public battleService: BattleService) {
         battleService.initTeam(
@@ -31,6 +34,15 @@ export class BattleComponent implements OnInit {
     }
 
     ngOnInit() {
+        this.socket.emit('event1', {
+            msg: 'Client to server, can you hear me server?'
+        });
 
+        this.socket.on('event2', (data: any) => {
+            console.log(data.msg);
+            this.socket.emit('event3', {
+                msg: 'Yes, its working for me!!'
+            });
+        });
     }
 }
